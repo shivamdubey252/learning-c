e5f4b0ce61c46ebdc39fd8c54a8c4c453ff63fdb
shivam <dubeshiv66@gmail.com>
2024-03-24 10:48:28 -0400

    switch case example added

---
+++ switch _exp.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+int opt=3;
+switch(opt)
+{
+case 1:
+printf("January\n:");
+break;
+case 2:
+printf("february\n:");
+break;
+default :
+printf("december:");
+break;
+}
+return 0;
+}
