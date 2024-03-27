cb2f4a18959c87a579fdf9afa152f9bda870e271
shivam <dubeshiv66@gmail.com>
2024-03-27 13:11:18 -0400

    if statement exp.c

---
+++ if statement exp.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+  int main()  {
+  int a;
+  printf("enter the value:");
+  scanf("%d",&a);
+       if(a%2==0)  {
+          printf("The given value is even");
+     }
+       else
+          printf("The given value is odd");
+  return 0;
+ }
