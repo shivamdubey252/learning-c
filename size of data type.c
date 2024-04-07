7e09e9f8034e558e1b7c7b92ffe2140b32f9a473
shivam <dubeshiv66@gmail.com>
2024-04-07 04:48:57 -0400

    size of data type

---
+++ size of data type.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+  int main()  {
+      int xint;
+      float yfloat;
+      char zchar;
+      printf("%l\n",sizeof(xint));
+      printf("%lu\n",sizeof(yfloat));
+      printf("%lu\n",sizeof(zchar));
+  return 0;
+}
