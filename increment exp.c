7dc0b0638bcedc06d241fe358ce0fa1becd8608d
shivam <dubeshiv66@gmail.com>
2024-04-08 11:03:39 -0400

    increment exp

---
+++ increment exp.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+ int main()  {
+      int i,n;
+      printf("Entre any number:");
+      scanf("%d",&n);
+         for(i=0;i<=n;i++)
+          {
+          printf("%d\n",i);
+          }
+ return 0;
+}
