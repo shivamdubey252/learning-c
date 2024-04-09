db928d6625e36cf94343a47e3835240fd5a72ead
shivam <dubeshiv66@gmail.com>
2024-04-09 12:35:13 -0400

    Conditions operator exp

---
+++ conditions operator exp.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+    int main()  {
+         int x,y;
+         printf("Enter the value:");
+         scanf("%d",&x);
+         printf("Enter any value:");
+         scanf("%d",&y);
+         if(x>=y)  {
+         printf("Keyboard");
+         }
+         else
+         printf("Mouse");
+    return 0;
+  }
