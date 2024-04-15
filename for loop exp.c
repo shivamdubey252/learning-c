eea96f799374846c6c0dce61042a4dd9db5e0fc4
shivam <dubeshiv66@gmail.com>
2024-04-15 12:58:33 -0400

    For loop exp

--- for loop exp.c
+++ for loop exp.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 
-   int main()   {
-       int n,i,sum=0;
-            printf("Enter the number:");
-            scanf("%d",&n);
-        for(i=0;i<=n;i++)
-        sum=sum+i;
-        printf("Added the number=%d",sum);
-   return 0;
+    int main()   {
+         int n,i,j;
+             printf("Enter the number:");
+             scanf("%d",&n);
+             for(i=0;i<=n;i++)
+             {
+             for(j=0;j<=n;j++)
+             printf("%d\n",i);
+             }
+    return 0;
   }
